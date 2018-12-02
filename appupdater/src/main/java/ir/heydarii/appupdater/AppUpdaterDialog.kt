3821b4a733074989d829661cf3dc6271fe25d8b7
Pouya <pouyaheydary@gmail.com>
2018-12-02 14:18:42 +0330

    added main dialog fragment style

--- appupdater/src/main/java/ir/heydarii/appupdater/AppUpdaterDialog.kt
+++ appupdater/src/main/java/ir/heydarii/appupdater/AppUpdaterDialog.kt
@@ -10,10 +10,7 @@ import androidx.fragment.app.DialogFragment
 
 class AppUpdaterDialog : DialogFragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_app_updater_dialog, container, false)
     }
