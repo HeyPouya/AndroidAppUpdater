e907f2108d882e5fa870c56c9b42d8df510f2e9b
Pouya <pouyaheydary@gmail.com>
2019-04-15 12:07:09 +0430

    changed constraint layout's version from 2-alpha to 1.1.3 - changed test library version - fixed an issue in instrumented test files

--- appupdater/src/androidTest/java/ir/heydarii/appupdater/ExampleInstrumentedTest.java
+++ appupdater/src/androidTest/java/ir/heydarii/appupdater/ExampleInstrumentedTest.java
@@ -2,8 +2,8 @@ package ir.heydarii.appupdater;
 
 import android.content.Context;
 import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
