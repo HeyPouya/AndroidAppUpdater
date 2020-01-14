d853c8abe716816828e9cf6df65c9f89cd2d09dd
Pouya <pouyaheydary@gmail.com>
2020-01-14 17:39:38 +0330

    Added an if condition to make sure nobody calls installAPKForPAndAbove on any pre P Android versions

--- appupdater/src/main/java/ir/heydarii/appupdater/utils/InstallAPKUtil.kt
+++ appupdater/src/main/java/ir/heydarii/appupdater/utils/InstallAPKUtil.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.core.content.FileProvider
 import java.io.File
 
@@ -18,34 +17,41 @@ class InstallAPKUtil {
      * Uses different methods to install the apk depending the user's Android version
      */
     fun installAPK(context: Context, path: String, androidVersion: Int) {
-        when (androidVersion) {
-            in 0..Build.VERSION_CODES.M -> installAPKForMAndBellow(context, path)
-            in Build.VERSION_CODES.N..Build.VERSION_CODES.O -> installAPKForNtoO(context, path)
-            else -> installAPKForPAndAbove(context, path)
+        when {
+            androidVersion in 0..Build.VERSION_CODES.M -> installAPKForMAndBellow(context, path)
+            androidVersion in Build.VERSION_CODES.N..Build.VERSION_CODES.O -> installAPKForNtoO(
+                context,
+                path
+            )
+            androidVersion >= Build.VERSION_CODES.P -> installAPKForPAndAbove(context, path)
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.P)
     private fun installAPKForPAndAbove(context: Context, path: String) {
-        if (context.packageManager.canRequestPackageInstalls()) {
-            try {
-                val intent = Intent(
-                    Intent.ACTION_VIEW,
-                    FileProvider.getUriForFile(
-                        context,
-                        "${context.packageName}.fileProvider.GenericFileProvider",
-                        File(path)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            if (context.packageManager.canRequestPackageInstalls()) {
+                try {
+                    val intent = Intent(
+                        Intent.ACTION_VIEW,
+                        FileProvider.getUriForFile(
+                            context,
+                            "${context.packageName}.fileProvider.GenericFileProvider",
+                            File(path)
+                        )
                     )
-                )
-                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                context.startActivity(intent)
-            } catch (e: Exception) {
-                Log.d(Constants.TAG, e.message.orEmpty())
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    context.startActivity(intent)
+                } catch (e: Exception) {
+                    Log.d(Constants.TAG, e.message.orEmpty())
+                }
+            } else {
+                UnknownSourceInstallRequest().showRequest(context)
             }
         } else {
-            UnknownSourceInstallRequest().showRequest(context)
+            Log.d(Constants.TAG, "Don't call this method on Android version bellow P (9)")
         }
+
     }
 
     private fun installAPKForNtoO(context: Context, path: String) {
