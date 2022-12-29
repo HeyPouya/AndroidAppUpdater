974db998a681a635060212e0569a854ad8658b7a
Pouya Heydari <pouya.heydari@free-now.com>
2022-12-29 19:14:33 +0100

    Added tests for CafeBazaarStore.kt

--- core/src/main/java/com/pouyaheydari/appupdater/core/stores/CafeBazaarStore.kt
+++ core/src/main/java/com/pouyaheydari/appupdater/core/stores/CafeBazaarStore.kt
@@ -6,17 +6,17 @@ import android.net.Uri
 import com.pouyaheydari.appupdater.core.pojo.Store
 import com.pouyaheydari.appupdater.core.pojo.UpdaterStoreList
 
-private const val BAZAAR_URL = "bazaar://details?id="
-private const val BAZAAR_PACKAGE = "com.farsitel.bazaar"
+const val BAZAAR_URL = "bazaar://details?id="
+const val BAZAAR_PACKAGE = "com.farsitel.bazaar"
 
 /**
  * shows apk in CafeBazaar store
  */
 class CafeBazaarStore : Stores() {
     override fun setStoreData(context: Context?, item: UpdaterStoreList) {
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse("$BAZAAR_URL${item.packageName}")
-        intent.setPackage(BAZAAR_PACKAGE)
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("$BAZAAR_URL${item.packageName}")).run {
+            setPackage(BAZAAR_PACKAGE)
+        }
         showStore(context, intent, item, Store.CAFE_BAZAAR)
     }
 }
