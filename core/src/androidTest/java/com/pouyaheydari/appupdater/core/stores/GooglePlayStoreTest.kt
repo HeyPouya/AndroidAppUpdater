7e80c8460d25b8962a4911dca3d03e03758251ea
Pouya Heydari <pouya.heydari@free-now.com>
2022-12-29 19:15:53 +0100

    Added tests for GooglePlayStore.kt

---
+++ core/src/androidTest/java/com/pouyaheydari/appupdater/core/stores/GooglePlayStoreTest.kt
@@ -0,0 +1,33 @@
+package com.pouyaheydari.appupdater.core.stores
+
+import android.net.Uri
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.rule.IntentsRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.pouyaheydari.appupdater.core.pojo.Store
+import com.pouyaheydari.appupdater.core.pojo.UpdaterStoreList
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+internal class GooglePlayStoreTest {
+
+    @get:Rule
+    val intentsTestRule = IntentsRule()
+
+    @Test
+    fun whenCalling_setStoreData_then_intentGetsFiredCorrectly() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val googlePlayStore = GooglePlayStore()
+        val packageName = appContext.packageName
+        val url = "https://pouyaheydari.com"
+
+        googlePlayStore.setStoreData(appContext, UpdaterStoreList(store = Store.GOOGLE_PLAY, packageName = packageName, url = url))
+
+        Intents.intended(IntentMatchers.hasPackage(PLAY_PACKAGE))
+        Intents.intended(IntentMatchers.hasData(Uri.parse("$PLAY_URL$packageName")))
+    }
+}
--- core/src/main/java/com/pouyaheydari/appupdater/core/stores/GooglePlayStore.kt
+++ core/src/main/java/com/pouyaheydari/appupdater/core/stores/GooglePlayStore.kt
@@ -6,7 +6,8 @@ import android.net.Uri
 import com.pouyaheydari.appupdater.core.pojo.Store
 import com.pouyaheydari.appupdater.core.pojo.UpdaterStoreList
 
-private const val PLAY_URL = "market://details?id="
+const val PLAY_URL = "market://details?id="
+const val PLAY_PACKAGE = "com.android.vending"
 
 /**
  * shows apk in GooglePlay store
@@ -14,7 +15,7 @@ private const val PLAY_URL = "market://details?id="
 class GooglePlayStore : Stores() {
     override fun setStoreData(context: Context?, item: UpdaterStoreList) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse("$PLAY_URL${item.packageName}")).run {
-            setPackage("com.android.vending")
+            setPackage(PLAY_PACKAGE)
         }
         showStore(context, intent, item, Store.GOOGLE_PLAY)
     }
