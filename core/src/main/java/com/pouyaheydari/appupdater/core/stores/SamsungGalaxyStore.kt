63c4aea7f741dcbee69f1bd85854245288bbad0e
Pouya Heydari <pouya.heydari@free-now.com>
2022-12-29 20:42:24 +0100

    Added test for SamsungGalaxyStore.kt

--- core/src/main/java/com/pouyaheydari/appupdater/core/stores/SamsungGalaxyStore.kt
+++ core/src/main/java/com/pouyaheydari/appupdater/core/stores/SamsungGalaxyStore.kt
@@ -6,14 +6,14 @@ import android.net.Uri
 import com.pouyaheydari.appupdater.core.pojo.Store
 import com.pouyaheydari.appupdater.core.pojo.UpdaterStoreList
 
-private const val GALAXY_STORE_URL = "samsungapps://ProductDetail/"
+const val SAMSUNG_GALAXY_STORE_URL = "samsungapps://ProductDetail/"
 
 /**
- * shows apk in Samsung Galaxy store
+ * shows apk in [Samsung Galaxy store](https://www.samsung.com/de/apps/galaxy-store/)
  */
 class SamsungGalaxyStore : Stores() {
     override fun setStoreData(context: Context?, item: UpdaterStoreList) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("$GALAXY_STORE_URL${item.packageName}"))
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("$SAMSUNG_GALAXY_STORE_URL${item.packageName}"))
         showStore(context, intent, item, Store.SAMSUNG_GALAXY_STORE)
     }
 }
