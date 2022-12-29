71625330652a5624093fc9e7bd2e5b6208b37cdd
Pouya Heydari <pouya.heydari@free-now.com>
2022-12-29 19:16:32 +0100

    Added tests for HuaweiAppGallery.kt

--- core/src/main/java/com/pouyaheydari/appupdater/core/stores/HuaweiAppGallery.kt
+++ core/src/main/java/com/pouyaheydari/appupdater/core/stores/HuaweiAppGallery.kt
@@ -6,14 +6,17 @@ import android.net.Uri
 import com.pouyaheydari.appupdater.core.pojo.Store
 import com.pouyaheydari.appupdater.core.pojo.UpdaterStoreList
 
-private const val APP_GALLERY_URL = "appmarket://details?id="
+const val HUAWEI_APP_GALLERY_URL = "appmarket://details?id="
+const val HUAWEI_APP_GALLERY_PACKAGE = "com.huawei.appmarket"
 
 /**
  * shows apk in Huawei App Gallery store
  */
 class HuaweiAppGallery : Stores() {
     override fun setStoreData(context: Context?, item: UpdaterStoreList) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("$APP_GALLERY_URL${item.packageName}"))
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("$HUAWEI_APP_GALLERY_URL${item.packageName}")).run {
+            setPackage(HUAWEI_APP_GALLERY_PACKAGE)
+        }
         showStore(context, intent, item, Store.HUAWEI_APP_GALLERY)
     }
 }
