b12d9d0d4886d3a11247dbdf9528fff75a7e0d14
Pouya Heydari <pouya.heydari@free-now.com>
2022-12-29 19:15:12 +0100

    Added tests for FDroid.kt

--- core/src/main/java/com/pouyaheydari/appupdater/core/stores/FDroid.kt
+++ core/src/main/java/com/pouyaheydari/appupdater/core/stores/FDroid.kt
@@ -6,14 +6,17 @@ import android.net.Uri
 import com.pouyaheydari.appupdater.core.pojo.Store
 import com.pouyaheydari.appupdater.core.pojo.UpdaterStoreList
 
-private const val FDROID_URL = "fdroid.app://details?id="
+const val FDROID_URL = "fdroid.app://details?id="
+const val FDROID_PACKAGE = "org.fdroid.fdroid"
 
 /**
  * shows apk in F-Droid store
  */
 class FDroid : Stores() {
     override fun setStoreData(context: Context?, item: UpdaterStoreList) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("$FDROID_URL${item.packageName}"))
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("$FDROID_URL${item.packageName}")).run {
+            setPackage(FDROID_PACKAGE)
+        }
         showStore(context, intent, item, Store.FDROID)
     }
 }
