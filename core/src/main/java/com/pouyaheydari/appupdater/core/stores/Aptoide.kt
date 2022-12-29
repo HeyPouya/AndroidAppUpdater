22012908914cef0a9502b5cbba1afbcfd3f98cf2
Pouya Heydari <pouya.heydari@free-now.com>
2022-12-29 19:13:57 +0100

    Added tests for Aptoide.kt

--- core/src/main/java/com/pouyaheydari/appupdater/core/stores/Aptoide.kt
+++ core/src/main/java/com/pouyaheydari/appupdater/core/stores/Aptoide.kt
@@ -6,14 +6,17 @@ import android.net.Uri
 import com.pouyaheydari.appupdater.core.pojo.Store
 import com.pouyaheydari.appupdater.core.pojo.UpdaterStoreList
 
-private const val APTOIDE_URL = "aptoideinstall://package="
+const val APTOIDE_URL = "aptoideinstall://package="
+const val APTOIDE_PACKAGE = "cm.aptoide.pt"
 
 /**
  * shows apk in Aptoide Store
  */
 class Aptoide : Stores() {
     override fun setStoreData(context: Context?, item: UpdaterStoreList) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("$APTOIDE_URL${item.packageName}"))
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("$APTOIDE_URL${item.packageName}")).run {
+            setPackage(APTOIDE_PACKAGE)
+        }
         showStore(context, intent, item, Store.APTOIDE)
     }
 }
