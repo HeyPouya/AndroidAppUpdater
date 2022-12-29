483997f2753872846ae89cd97a84bf2edee5befe
Pouya Heydari <pouya.heydari@free-now.com>
2022-12-29 19:13:21 +0100

    Added tests for AmazonAppStore.kt

--- core/src/main/java/com/pouyaheydari/appupdater/core/stores/AmazonAppStore.kt
+++ core/src/main/java/com/pouyaheydari/appupdater/core/stores/AmazonAppStore.kt
@@ -6,14 +6,17 @@ import android.net.Uri
 import com.pouyaheydari.appupdater.core.pojo.Store
 import com.pouyaheydari.appupdater.core.pojo.UpdaterStoreList
 
-private const val AMAZON_APP_STORE_URL = "amzn://apps/android?p="
+const val AMAZON_APP_STORE_URL = "amzn://apps/android?p="
+const val AMAZON_PACKAGE = "com.amazon.venezia"
 
 /**
  * shows apk in Amazon App Store
  */
 class AmazonAppStore : Stores() {
     override fun setStoreData(context: Context?, item: UpdaterStoreList) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("$AMAZON_APP_STORE_URL${item.packageName}"))
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("$AMAZON_APP_STORE_URL${item.packageName}")).run {
+            setPackage(AMAZON_PACKAGE)
+        }
         showStore(context, intent, item, Store.AMAZON_APP_STORE)
     }
 }
