d9b5585d82815ea23fb1cc436b886c6ee5b408ce
Pouya Heydari <pouya.heydari@free-now.com>
2023-01-01 17:20:54 +0100

    Used viewBinding in appupdater module

--- appupdater/src/main/java/com/pouyaheydari/appupdater/UpdateInProgressDialog.kt
+++ appupdater/src/main/java/com/pouyaheydari/appupdater/UpdateInProgressDialog.kt
@@ -1,17 +1,27 @@
 package com.pouyaheydari.appupdater
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
+import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams.MATCH_PARENT
 import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams.WRAP_CONTENT
 import androidx.fragment.app.DialogFragment
 import com.pouyaheydari.appupdater.core.utils.tf
+import com.pouyaheydari.appupdater.databinding.FragmentUpdateInProgressDialogBinding
 
 /**
- * Dialog to show download progress to user
+ * Dialog to show download in progress to user
  */
-internal class UpdateInProgressDialog : DialogFragment(R.layout.fragment_update_in_progress_dialog) {
+internal class UpdateInProgressDialog : DialogFragment() {
+
+    private var _binding: FragmentUpdateInProgressDialogBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentUpdateInProgressDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     companion object {
         var instance = UpdateInProgressDialog()
@@ -19,10 +29,10 @@ internal class UpdateInProgressDialog : DialogFragment(R.layout.fragment_update_
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(view) {
+        with(binding) {
             tf?.let {
-                findViewById<TextView>(R.id.txtTitle).typeface = it
-                findViewById<TextView>(R.id.txtDescription).typeface = it
+                txtTitle.typeface = it
+                txtDescription.typeface = it
             }
         }
     }
@@ -31,5 +41,10 @@ internal class UpdateInProgressDialog : DialogFragment(R.layout.fragment_update_
         super.onStart()
         // making width of dialog to match_parent
         dialog?.window?.setLayout(MATCH_PARENT, WRAP_CONTENT)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
