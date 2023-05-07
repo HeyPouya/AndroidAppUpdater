d019dbf4cf270e34f18a6f6978b8738dfe40ee2a
Pouya Heydari <pouya.heydari@free-now.com>
2023-05-07 23:03:48 +0200

    Mde compose and kotlin sample colors identical

--- app/src/main/java/com/pouyaheydari/androidappupdater/ui/compose/theme/Color.kt
+++ app/src/main/java/com/pouyaheydari/androidappupdater/ui/compose/theme/Color.kt
@@ -2,8 +2,5 @@ package com.pouyaheydari.androidappupdater.ui.compose.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val green = Color(0xFF008577)
+val darkGreen = Color(0xFF00574B)
