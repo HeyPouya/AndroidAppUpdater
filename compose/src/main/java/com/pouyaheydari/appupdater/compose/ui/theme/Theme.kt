2889c24a3f57b78fc16ee174fdcd51472e95c822
Pouya Heydari <pouya.heydari@free-now.com>
2023-05-07 23:11:27 +0200

    Removed setting status bar color from the compose library

--- compose/src/main/java/com/pouyaheydari/appupdater/compose/ui/theme/Theme.kt
+++ compose/src/main/java/com/pouyaheydari/appupdater/compose/ui/theme/Theme.kt
@@ -1,16 +1,11 @@
 package com.pouyaheydari.appupdater.compose.ui.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColors(
     onSurface = White80,
@@ -33,16 +28,6 @@ fun AndroidAppUpdaterTheme(
     val colorScheme = when {
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val currentWindow = (view.context as? Activity)?.window
-                ?: throw Exception("Not in an activity - unable to get Window reference")
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars =
-                darkTheme
-        }
     }
 
     MaterialTheme(
