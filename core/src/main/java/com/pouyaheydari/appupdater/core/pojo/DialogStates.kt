aaaede166a84f576692e72bb2b171a81f3a48552
Pouya Heydari <pouya.heydari@free-now.com>
2023-09-10 17:06:08 +0200

    Upgraded compose to the latest version

--- core/src/main/java/com/pouyaheydari/appupdater/core/pojo/DialogStates.kt
+++ core/src/main/java/com/pouyaheydari/appupdater/core/pojo/DialogStates.kt
@@ -6,8 +6,8 @@ import com.pouyaheydari.appupdater.core.stores.Stores
  * Represents UI states of the update dialog
  */
 sealed interface DialogStates {
-    object ShowUpdateInProgress : DialogStates
-    object HideUpdateInProgress : DialogStates
+    data object ShowUpdateInProgress : DialogStates
+    data object HideUpdateInProgress : DialogStates
     data class OpenStore(val store: Stores?) : DialogStates
     data class DownloadApk(val apkUrl: String) : DialogStates
 }
