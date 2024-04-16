8ee1932117ade6cf4d380a553c152b8f296d7602
Pouya Heydari <pouya.heydari@free-now.com>
2024-04-16 18:27:46 +0200

    Removed throwing exception

--- app/src/main/java/com/pouyaheydari/androidappupdater/ui/compose/theme/Theme.kt
+++ app/src/main/java/com/pouyaheydari/androidappupdater/ui/compose/theme/Theme.kt
@@ -33,11 +33,9 @@ internal fun AndroidAppUpdaterTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            val currentWindow = (view.context as? Activity)?.window
-                ?: throw Exception("Not in an activity - unable to get Window reference")
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars =
-                darkTheme
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
