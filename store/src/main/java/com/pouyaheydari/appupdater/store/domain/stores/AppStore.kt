e923c73f374eb4f9631d6d192f6b5ec322cdfca3
Pouya Heydari <pouya.heydari@free-now.com>
2024-04-25 20:47:43 +0200

    Removed sealed to prevent this error while deploying the library: PermittedSubclasses requires ASM9

--- store/src/main/java/com/pouyaheydari/appupdater/store/domain/stores/AppStore.kt
+++ store/src/main/java/com/pouyaheydari/appupdater/store/domain/stores/AppStore.kt
@@ -3,6 +3,6 @@ package com.pouyaheydari.appupdater.store.domain.stores
 import android.content.Intent
 import android.os.Parcelable
 
-sealed interface AppStore : Parcelable {
+interface AppStore : Parcelable {
     fun getIntent(): Intent
 }
