bdb4e508ecd11198a59616b3cf15c6636e93f596
Pouya Heydari <pouya.heydari@sumup.com>
2024-07-12 18:27:03 +0200

    Added maven publish gradle dependency and added usage for core

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.maven.publish) apply false
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,23 +1,4 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    `java-library`
-    `maven-publish`
-}
-
-java {
-    withJavadocJar()
-    withSourcesJar()
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                from(components["java"])
-                groupId = "com.pouyaheydari.updater"
-                artifactId = "core"
-                version = libs.versions.appVersion.get()
-            }
-        }
-    }
+    alias(libs.plugins.maven.publish)
 }
