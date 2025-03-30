eca026f7048780a0db1c4eab61bcdae3ae245035
Pouya Heydari <pouya.heydari@sumup.com>
2025-03-30 17:19:07 +0200

    Added unit tests for appupdater module

--- appupdater/build.gradle.kts
+++ appupdater/build.gradle.kts
@@ -49,6 +49,9 @@ dependencies {
 
     // testing dependency
     testImplementation(libs.junit4)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.turbine)
+    testImplementation (libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.test.junit)
     androidTestImplementation(libs.androidx.test.rules)
     androidTestImplementation(libs.androidx.test.ui.espresso.core)
