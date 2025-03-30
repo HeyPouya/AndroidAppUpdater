5bd993989f8af5bd2d7c5d5485533be09b4d3c14
Pouya Heydari <pouya.heydari@sumup.com>
2025-03-30 17:30:38 +0200

    Fixed deprecated status bar color attribute in the demo app's theme file

--- app/src/main/java/com/pouyaheydari/appupdater/demo/ui/compose/theme/Theme.kt
+++ app/src/main/java/com/pouyaheydari/appupdater/demo/ui/compose/theme/Theme.kt
@@ -1,15 +1,10 @@
 package com.pouyaheydari.appupdater.demo.ui.compose.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = green,
@@ -22,26 +17,10 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-internal fun AndroidAppUpdaterTheme(
+fun AndroidAppUpdaterTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content,
-    )
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+    MaterialTheme(colorScheme = colorScheme, typography = Typography, content = content)
 }
