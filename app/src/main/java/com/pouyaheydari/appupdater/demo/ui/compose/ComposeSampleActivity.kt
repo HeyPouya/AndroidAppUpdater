24836ab473a62eb51f4b60cccbbd6383024d4522
Pouya Heydari <pouya.heydari@sumup.com>
2025-03-30 17:41:35 +0200

    Updated styles for the demo app

--- app/src/main/java/com/pouyaheydari/appupdater/demo/ui/compose/ComposeSampleActivity.kt
+++ app/src/main/java/com/pouyaheydari/appupdater/demo/ui/compose/ComposeSampleActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,7 +42,7 @@ internal class ComposeSampleActivity : ComponentActivity() {
 
                 Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                     Button(onClick = { state = true }) {
-                        Text(text = getString(R.string.show_compose_dialog))
+                        Text(text = getString(R.string.show_compose_dialog), color = Color.White)
                     }
                 }
                 if (state) {
--- app/src/main/java/com/pouyaheydari/appupdater/demo/ui/compose/theme/Theme.kt
+++ app/src/main/java/com/pouyaheydari/appupdater/demo/ui/compose/theme/Theme.kt
@@ -5,15 +5,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
-    primary = green,
-    secondary = darkGreen,
+    primary = Color.Black,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = green,
-    secondary = darkGreen,
+    primary = Color.Black,
 )
 
 @Composable
