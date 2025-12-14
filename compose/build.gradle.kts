37d3c68104a61ee8561d315a820cd825335eb4f6
Pouya Heydari <pouya.heydari@sumup.com>
2025-12-14 18:43:17 +0100

    Updated dependencies to the latest version

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+    kotlin {
+        jvmToolchain(17)
     }
     buildFeatures {
         compose = true
